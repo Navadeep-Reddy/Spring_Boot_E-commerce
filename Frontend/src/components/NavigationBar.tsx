382e75d2c0c18067868441fe4886ac50c28e8695
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2025-05-27 17:34:35 +0530

    Added slight tweaks

--- Frontend/src/components/NavigationBar.tsx
+++ Frontend/src/components/NavigationBar.tsx
@@ -5,9 +5,9 @@ function NavigationBar() {
     <nav className="fixed top-0 w-[100%] shadow-sm flex items-center px-10 justify-between bg-neutral">
       <h1 className="text-3xl font-extrabold text-white">Fastify</h1>
       <ul className="flex  justify-center gap-x-5 font-bold  text-[20px] text-white my-5">
-        <li className="hover:text-secondary duration-100">Home</li>
-        <li className="hover:text-secondary duration-100">Add Product</li>
-        <li className="hover:text-secondary duration-100">Categories</li>
+        <li className="hover:text-secondary duration-100 cursor-pointer">Home</li>
+        <li className="hover:text-secondary duration-100 cursor-pointer">Add Product</li>
+        <li className="hover:text-secondary duration-100 cursor-pointer">Categories</li>
       </ul>
       <div className="flex gap-x-2">
         <Button className="rounded-4xl bg-primary text-white">Search</Button>
