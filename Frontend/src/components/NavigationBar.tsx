93cac4420f1b0bf2d9c502d04b4ff9b34cc71048
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2025-05-27 17:46:13 +0530

    Made the navigation bar sticky

--- Frontend/src/components/NavigationBar.tsx
+++ Frontend/src/components/NavigationBar.tsx
@@ -1,17 +1,40 @@
+import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 function NavigationBar() {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const setter = () => {
+      setIsSticky(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", setter);
+
+    return () => window.removeEventListener("scroll", setter);
+  }, []);
+
   return (
-    <nav className="fixed top-0 w-[100%] shadow-sm flex items-center px-10 justify-between bg-neutral">
-      <h1 className="text-3xl font-extrabold text-white">Fastify</h1>
+    <nav
+      className={`fixed top-0 w-[100%] ${
+        isSticky ? "shadow-xl" : "shadow-sm"
+      } flex items-center px-10 justify-between bg-neutral`}
+    >
+      <h1 className="text-3xl font-extrabold text-white">RandomCommerce</h1>
       <ul className="flex  justify-center gap-x-5 font-bold  text-[20px] text-white my-5">
-        <li className="hover:text-secondary duration-100 cursor-pointer">Home</li>
-        <li className="hover:text-secondary duration-100 cursor-pointer">Add Product</li>
-        <li className="hover:text-secondary duration-100 cursor-pointer">Categories</li>
+        <li className="hover:text-secondary duration-100 cursor-pointer">
+          Home
+        </li>
+        <li className="hover:text-secondary duration-100 cursor-pointer">
+          Add Product
+        </li>
+        <li className="hover:text-secondary duration-100 cursor-pointer">
+          Categories
+        </li>
       </ul>
       <div className="flex gap-x-2">
         <Button className="rounded-4xl bg-primary text-white">Search</Button>
-        <Input className="bg-secondary" />
+        <Input placeholder="Enter product" className="bg-secondary" />
       </div>
     </nav>
   );
